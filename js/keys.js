c8c45811b88252a08b8516e022b6c5cc92c76c01
genesiscruzasturias <140553806+genesiscruzasturias@users.noreply.github.com>
2024-01-31 12:46:06 -0600

    Delete js/keys.js

--- js/keys.js
+++
@@ -1,6 +0,0 @@
-const keys = {
-    mapbox: "pk.eyJ1IjoiZ2VuZXNpc2NydXphc3R1cmlhcyIsImEiOiJjbG1mOGo2ZTcxaDF2M3NwOHgwZG95emg2In0.e_77z9_2a1HIr9uRgkO45A",
-    weatherMap: "7486d4af177c035e823d5cfd234b1398",
-}
-
-export default keys;
